ab0145621f9176cb40907675f2a535214479a8a0
surijatin <surijatin16@gmail.com>
2024-09-28 00:43:21 -0400

    WIP header

--- src/layout/HeaderComponent.tsx
+++ src/layout/HeaderComponent.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Avatar } from "antd";
+import { ShoppingCartOutlined } from "@ant-design/icons";
+import { Badge } from "antd";
+import { Drawer } from "antd";
 
 const Header = () => {
+  const [drawerVisible, setDrawerVisible] = useState(false);
+
+  const showDrawer = () => {
+    setDrawerVisible(true);
+  };
+
+  const closeDrawer = () => {
+    setDrawerVisible(false);
+  };
+
   return (
-    <div className="border-b flex px-16 pt-4">
-      <div className="w-full flex justify-center md:justify-start">
+    <div className="border-b flex px-4 md:px-16 pt-4">
+      <div className="w-full flex justify-start">
         <img src="/logo.png" alt="Instamart Logo" className="h-24" />
+      </div>
+      <div className="ml-auto flex items-center md:hidden">
+        <Badge count={1}>
+          <Avatar
+            icon={<ShoppingCartOutlined />}
+            size={"large"}
+            onClick={showDrawer}
+          />
+        </Badge>
       </div>
       <div className="ml-auto flex items-center hidden md:flex">
         <Avatar
@@ -17,6 +39,15 @@ const Header = () => {
           <span className="text-gray-800 font-semibold">Jatin Suri</span>
           <span className="text-gray-800 text-sm">surijatin16@gmail.com</span>
         </div>
+        <Drawer
+          title="Basic Drawer"
+          onClose={closeDrawer}
+          visible={drawerVisible}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Drawer>
       </div>
     </div>
   );
